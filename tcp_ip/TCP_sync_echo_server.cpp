ba11f9c82a2eea0bb59822fd8a87171e57900178
NineHearts <46129253+NineHearts@users.noreply.github.com>
2023-11-23 12:20:16 +0900

    simple sync echo server

--- tcp_ip/TCP_sync_echo_server.cpp
+++ tcp_ip/TCP_sync_echo_server.cpp
@@ -17,5 +17,44 @@ int main()
     boost::asio::ip::tcp::socket socket(io_service);
     // 소켓과 acceptor 연결 
     acceptor.accept(socket);
+
+    // 동기 서버이므로 지속적인 통신을 하기 위한 반복문
+    while(true)
+    {
+        // 클라이언트가 보낸 문자를 담을 버퍼
+        //std::array<char, 128> buf{};
+        char ch;
+        
+        // 에러 코드
+        boost::system::error_code ec;
+
+        // 클라이언트와 연결된 소켓으로부터 데이터를 읽고, 에러가 발생했으면 에러 코드를 받아옴
+        //size_t len = socket.read_some(boost::asio::buffer(buf), ec);
+        size_t len = socket.read_some(boost::asio::buffer(&ch, 1), ec);
+
+        // 에러 발생!
+        if (ec)
+        {
+            // 클라이언트와 연결이 끊길 경우 end of file 에러
+            if (ec == boost::asio::error::eof)
+            {
+                std::cout << "Disconnected from client..." << std::endl;
+            }
+            // 다른 에러일 경우 에러의 정보 표시
+            else
+            {
+                std::cout << "error No: " << ec.value() << " error Message: " << ec.message() << std::endl;
+            }
+            break;
+        }
+
+        std::cout << "클라이언트에서 받은 메세지: " << ch << std::endl;
+
+        // 데이터를 클라이언트로 보낼 경우 에러가 발생할 수 있지만 무시
+		boost::system::error_code ignored_error;
+        // 클라이언트에서 받은 문자를 버퍼에 담아 전송
+		socket.write_some(boost::asio::buffer(&ch, 1), ignored_error);
+    }
+
     return 0;
 }
